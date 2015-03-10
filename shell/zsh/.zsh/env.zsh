46a7e4902deb00774e59257d5b94ef181fd74b87
nakal <nakal@users.noreply.github.com>
2015-03-10 09:25:38 +0100

    vim background fix

--- shell/zsh/.zsh/env.zsh
+++ shell/zsh/.zsh/env.zsh
@@ -2,9 +2,14 @@
 
 export EDITOR='vim'
 export PAGER='less'
-export TERM='xterm-256color'
 export BLOCKSIZE='K'
 export LSCOLORS='gxfxcxdxbxegedabagacad'
 export GOROOT='/usr/local/go'
 
 source ~/.zsh/path.zsh
+
+if [ -z "$TMUX" ]; then
+	export TERM='xterm-256color'
+else
+	export TERM='screen-256color'
+fi
