962132bdb84e25a4ebe2c7851252d6aaf5f94f99
nakal <nakal@users.noreply.github.com>
2015-03-15 22:08:42 +0100

    Start CtrlP from command line

--- shell/zsh/.zsh/kbd.zsh
+++ shell/zsh/.zsh/kbd.zsh
@@ -7,3 +7,11 @@ bindkey '\e\e[C' forward-word
 bindkey '\e[3~' delete-char
 bindkey '\e[1~' beginning-of-line
 bindkey '\e[4~' end-of-line
+
+# CtrlP from zsh
+zsh_ctrlp() {
+	(unset TMUX; xterm -e tmux -2 new-session 'vim -c CtrlP' 2>/dev/null) &
+}
+zle -N zsh_ctrlp
+
+bindkey "^p" zsh_ctrlp
