4941c84bc4e9155f88b522348f81e74f8fbcb8dc
nakal <nakal@users.noreply.github.com>
2015-03-20 11:23:35 +0100

    Improve completion colors

--- shell/zsh/.zsh/completion.zsh
+++ shell/zsh/.zsh/completion.zsh
@@ -15,10 +15,15 @@ zmodload -i zsh/complist
 
 # directories cyan, executables red, links purple
 zstyle ':completion:*:default' list-colors 'di=36' 'ex=31' 'ln=35'
-zstyle ':completion:*:(aliases|builtins|commands)' list-colors '=*=0;31'
-zstyle ':completion:*:*:kill:*' list-colors '=(#b)(#s) #([0-9]##)* \(([^ ]##)\)*=0=31=33' '=(#b)(#s) #([0-9]##) ([^ ]##)*=0=31=33'
-zstyle ':completion:*:(options|values)' list-colors '=(#b)(#s)(-*) -- *=1;0=31;1'
+
+# for commands
+# candidates yellow
+# optional emphasis red (e.g. kill process name)
+zstyle ':completion:*:*:kill:*' list-colors '=(#b) #([0-9]##)* \(([^ ]##)\)*=0=33=31' '=(#b) #([0-9]##) ([^ ]##)*=0=33=31'
+zstyle ':completion:*:(builtins|commands|options)' list-colors '=(#b)(-*) -- *=0=33' '=^(-- )*=33'
+zstyle ':completion:*:aliases' list-colors '=*=33'
+zstyle ':completion:*:*:git:*:aliases' list-colors '=(#b)* (-- *)=33=0'
 
 # Completion commands
 zstyle ':completion:*:*:kill:*:processes' command 'ps -x -o pid= -o command='
-zstyle ':completion:*:*:kill:*' ignored-patterns '(#s)0(#e)'
+zstyle ':completion:*:*:kill:*' ignored-patterns '0'
