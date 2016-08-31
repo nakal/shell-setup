c15e06183976654c130ce0e9e050011e4ea0b727
nakal <nakal@users.noreply.github.com>
2016-08-31 15:56:51 +0200

    Remove custom setting of terminal properties

--- shell/zsh/.zsh/env.zsh
+++ shell/zsh/.zsh/env.zsh
@@ -6,8 +6,10 @@ export BLOCKSIZE='K'
 export LSCOLORS='gxfxcxdxbxegedabagacad'
 export GOROOT='/usr/local/go'
 
+source ~/.zsh/path.zsh
+
 if [ -n "$DISPLAY" ]; then
-	for termtype in xterm urxvt; do
+	for termtype in urxvt xterm; do
 		which -s $termtype > /dev/null
 		if [ $? -eq 0 ] && [ -z "$DEFAULT_X_TERMINAL" ]; then
 			export DEFAULT_X_TERMINAL="$termtype"
@@ -23,14 +25,6 @@ export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
 export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
 export LESS_TERMCAP_so=$'\E[38;5;016m\E[48;5;220m'    # begin standout-mode - info box
 export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
-
-source ~/.zsh/path.zsh
-
-if [ -z "$TMUX" ]; then
-	export TERM='xterm-256color'
-else
-	export TERM='screen-256color'
-fi
 
 # GPG agent
 GPG_TTY=$(tty)
