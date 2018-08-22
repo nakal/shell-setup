181a9178c7f5503df43de05fa1a5022cddaae9ee
nakal <nakal@users.noreply.github.com>
2018-08-22 16:05:22 +0200

    Update PATH environment

--- shell/zsh/.zsh/path.zsh
+++ shell/zsh/.zsh/path.zsh
@@ -1,4 +1,4 @@
 # System execution path
 
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:\
-/usr/X11R6/bin:$HOME/.local/bin:/usr/local/bin:$HOME/bin"
+$HOME/.local/bin:$HOME/.cabal/bin:/usr/local/bin:$HOME/bin"
